ef96ca44876abc2bb919b20935a08ce78d35e534
gabrielbocato-gif <gabriel.bocato@estudante.ifms.edu.br>
2026-03-30 12:14:17 -0400

    verificação de inputs e base inicial para preenchimento do crachá

--- script.js
+++ script.js
@@ -4,14 +4,52 @@ const curso = document.querySelector("#curso");
 const nascimento = document.querySelector("#data_nascimento");
 const telefone = document.querySelector("#telefone");
 const foto = document.querySelector("#fotosua");
-const input = querySelector("input")
 
-addEventListener("click", () => {
-    if (input.value == "") {
-        input.classList.add('vazio')
+enviar.addEventListener("click", () => {
+    if (nome.value == "") {
+        {
+            nome.classList.add('vazio')
+        }
     } else {
-        input.classList.remove('vazio')
+        nome.classList.remove('vazio')
     }
 
-    
+    if (curso.value == "") {
+        {
+            curso.classList.add('vazio')
+        }
+    } else {
+        curso.classList.remove('vazio')
+    }
+
+    if (nascimento.value == "") {
+        {
+            nascimento.classList.add('vazio')
+        }
+    } else {
+        nascimento.classList.remove('vazio')
+    }
+
+    if (telefone.value == "") {
+        {
+            telefone.classList.add('vazio')
+        }
+    } else {
+        telefone.classList.remove('vazio')
+    }
+
+    if (foto.value == "") {
+        {
+            foto.classList.add('vazio')
+        }
+    } else {
+        foto.classList.remove('vazio')
+    }
+
+    console.log(nome.value)
+    console.log(curso.value)
+    console.log(nascimento.value)
+    console.log(telefone.value)
+    console.log(foto.value)
+
 })
